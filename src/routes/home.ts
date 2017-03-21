7131cf4fa74be716118925183b8cca79dc9b60f9
unknown <>
2017-03-21 12:40:16 +0800

    标签重写,前端全部用ajax发请求

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -6,15 +6,25 @@ var router = express.Router();
 router.get('/', async function (req, res, next) {
     var _id = req.query._id;
     if (_id) {
-        var player = await playerModel.findOne().populate('currentRecord').exec();
-        var toPlayer;
-        if (player.currentRecord.toPlayer) {
-            toPlayer = await playerModel.find({ _id: player.currentRecord.toPlayer });
+        var player = await playerModel.findOne({ _id }).populate('currentRecord').exec();
+        console.log(player);
+        if (player) {
+            var toPlayer;
+            if (player.currentRecord) {
+                if (player.currentRecord.toPlayer)
+                    toPlayer = await playerModel.find({ _id: player.currentRecord.toPlayer }).exec();
+            }
+            res.render('home', {
+                player: player,
+                toPlayer: toPlayer
+            });
         }
-        res.render('home', {
-            player: player,
-            toPlayer: toPlayer
-        });
+        else {
+            res.json({
+                issuccess: true,
+                data: '错误'
+            })
+        }
     } else {
         res.json({
             issuccess: false,
--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -5,18 +5,28 @@ var router = express.Router();
 
 
 router.post('/', async (req, res, next) => {
-    req.body.age = parseInt(req.body.age);
-    var newPlayer = await new playerModel(req.body).save();
 
-    var currentRecord = await new recordModel({ playerId: newPlayer._id }).save();
-    newPlayer.currentRecord = currentRecord._id;
-    res.locals._id = newPlayer._id;
-    // 本地存储用户_id
-    res.json({
-        issucess: true,
-        data: newPlayer._id
-    });
-    // next();
+    var isExisit = await playerModel.findOne({ phone: req.body.phone }).count();
+    console.log(req.body);
+    if (isExisit) {
+        res.json({
+            issuccess: false,
+            data: '该手机号已经注册'
+        });
+    } else {
+        req.body.age = parseInt(req.body.age);
+        var newPlayer = await new playerModel(req.body).save();
+
+        var currentRecord = await new recordModel({ playerId: newPlayer._id }).save();
+        newPlayer.currentRecord = currentRecord._id;
+        res.locals._id = newPlayer._id;
+        // 本地存储用户_id
+        res.json({
+            issuccess: true,
+            data: newPlayer._id
+        });
+    }
+
 });
 
 module.exports = router;
